87c7990f0d8f2c866dc397c6fc85eb2fda531216
Liu Xuhan <liuxuhan2008@gmail.com>
2018-02-26 22:19:10 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# miniproject1
+# Mini Project 1
+
+Data visualization for commits on certain Git Repos. 
+
+## Getting Started
+
+This is a pure python 3 project. First part is data extraction via Git API.
+And second part is data visualization to display three charts.
+
+## Prerequisites
+
+Install python 3 and requirements.
+
+```
+pip install requirements.txt
+```
+
+## Running the data extraction
+
+```
+python git_api.py
+```
+Data will be downloaded into 'data' folder
+
+## Running the data visualization
+
+## Authors
+
+Liu Xuhan & Zhang Chao
--- git_api.py
+++ git_api.py
@@ -1,24 +1,35 @@
 import requests
 import json
 import csv
+AUTHOR = 'torvalds'
+REPO = 'linux'
 
 
 def weekly_commit():
-    url = 'https://api.github.com/repos/torvalds/linux/stats/participation'
+    url = 'https://api.github.com/repos/{}/{}/stats/participation'.format(
+        AUTHOR, REPO)
     response = requests.get(url, verify=False).json()
     with open('data/weekly_commit_52_week.txt', 'w') as json_file:
         json.dump(response, json_file)
 
-    data = open('data/weekly_commit_52_week.csv', 'w',newline='')
+    data = open('data/weekly_commit_52_week.csv', 'w', newline='')
     csvwriter = csv.writer(data)
     for index in range(53):
         if index == 0:
             all = response['all']
             owner = response['owner']
-            csvwriter.writerow(['week','all','owner'])
+            csvwriter.writerow(['week', 'all', 'owner'])
         csvwriter.writerow([index, all[index-1], owner[index-1]])
     data.close()
 
 
+def commit_per_hour():
+    url = 'https://api.github.com/repos/{}/{}/stats/punch_card'.format(
+        AUTHOR, REPO)
+    response = requests.get(url, verify=False).json()
+    print(json.dumps(response, indent=4))
+
+
 if __name__ == "__main__":
-    weekly_commit()
+    # weekly_commit()
+    commit_per_hour()
